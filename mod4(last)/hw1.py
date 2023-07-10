6e73f5f715ee6ca0df7f175c0c7610024fc610c6
nxzdix <nxzdix@gmail.com>
2023-07-10 22:40:47 +0300

    homework

--- mod4(last)/hw1.py
+++ mod4(last)/hw1.py
@@ -1,6 +1,78 @@
-word = str(input())
-word2 = word[::-1]
-if word == word2:
-  print('True')
-else:
-  print('False')
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>my first html page </title>
+</head>
+<body>
+<h1>Заголовок 1 уровня </h1>
+<h2>Заголовок 2 уровня </h2>
+<h3>Заголовок 3 уровня</h3>
+<h4>Заголовок 4 уровня</h4>
+<h5>Заголовок 5 уровня</h5>
+<h6>Заголовок 6 уровня</h6>
+
+<p class="paragraph-1">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Adipisci eveniet facere in magni modi necessitatibus odio, quo quod repudiandae unde!
+    Dicta enim esse facere magnam magni perferendis repellat tenetur totam?
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Adipisci eveniet facere in magni modi necessitatibus odio, quo quod repudiandae unde!
+    Dicta enim esse facere magnam magni perferendis repellat tenetur totam?
+</p>
+
+<p class="paragraph-2">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Animi aspernatur dolor dolores eveniet facilis harum id iure laudantium molestiae,
+    molestias mollitia natus necessitatibus, quisquam ratione rem repudiandae tempore veniam voluptate?
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Animi aspernatur dolor dolores eveniet facilis harum id iure laudantium molestiae,
+    molestias mollitia natus necessitatibus, quisquam ratione rem repudiandae tempore veniam voluptate?
+</p>
+
+<p class='paragraph-3'>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Adipisci amet animi consequuntur delectus dicta eaque eos,
+    error hic laboriosam libero maiores minima modi perspiciatis possimus sapiente sit sunt, temporibus velit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Adipisci amet animi consequuntur delectus dicta eaque eos,
+    error hic laboriosam libero maiores minima modi perspiciatis possimus sapiente sit sunt, temporibus velit.
+</p>
+
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Ad adipisci aperiam at aut commodi consequatur dolore earum, iste magnam,
+    nesciunt officia, qui quod reprehenderit sit voluptate.<span class="info">  Odit placeat temporibus velit.</span>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Ad adipisci aperiam at aut commodi consequatur dolore earum, iste magnam,
+    nesciunt officia, qui quod reprehenderit sit voluptate. Odit placeat temporibus velit.
+</p>
+
+<span>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Ad adipisci aperiam at aut commodi consequatur dolore earum, iste magnam,
+    nesciunt officia, qui quod reprehenderit sit voluptate. Odit placeat temporibus velit.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Ad adipisci aperiam at aut commodi consequatur dolore earum, iste magnam,
+    nesciunt officia, qui quod reprehenderit sit voluptate. Odit placeat temporibus velit.
+</span>
+
+<div>
+<span>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Ad adipisci aperiam at aut commodi consequatur dolore earum, iste magnam,
+    nesciunt officia, qui quod reprehenderit sit voluptate. Odit placeat temporibus velit.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Ad adipisci aperiam at aut commodi consequatur dolore earum, iste magnam,
+    nesciunt officia, qui quod reprehenderit sit voluptate. Odit placeat temporibus velit.
+</span>
+</div>
+
+<center>
+<p><img src="https://i.pinimg.com/564x/07/06/99/0706999950c0b80664d759620bcbf39c.jpg"></p>
+</center>>
+</body>
+</html>
